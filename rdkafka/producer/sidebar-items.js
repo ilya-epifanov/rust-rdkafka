75f4d47ee23821e77c5e28f3f5e932b1e3c4d1ff
Federico Giraud <fgiraud@yelp.com>
2018-03-16 14:53:58 +0100

    Update documentation

--- rdkafka/producer/sidebar-items.js
+++ rdkafka/producer/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["base_producer","Low level Kafka producers."],["future_producer",""]]});
+initSidebarItems({"mod":[["base_producer","Low level Kafka producers."],["future_producer","Future producer"]]});
